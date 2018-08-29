620121d890a86adfa2fd58f9f5a91fe925db31c7
Jiaxin <905284679@qq.com>
2018-08-29 17:03:47 +0800

    Add files via upload

---
+++ Day16/resume.js
@@ -0,0 +1,10 @@
+function showHobby()
+{
+	document.getElementById("myhobby").innerHTML="我爱代码，代码爱我。"
+}
+
+function hello()
+{
+	var name=document.getElementById("name").value;
+	alert("hello "+name);
+}
